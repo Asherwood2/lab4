c0174053e50ee8ec8e008cf835017a42c1a81013
Austin Sherwood <asherwood349@cub.uca.edu>
2025-04-11 14:51:17 -0500

    inserted data from old runnable file that had gui code

--- src/guiFoundation.java
+++ src/guiFoundation.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class guiFoundation {
         private JFrame frame;
@@ -13,5 +14,63 @@ public class guiFoundation {
 
             tablePanel = new TablePanel(calculation.getColumns(), calculation.getData());
             part2 = new Part2Panel(calculation.getListOfObjects());
+        }
+
+        private void creategui() {
+            setupFrame();
+            buildPanels();
+            setPanels();
+            finalizegui();
+        }
+
+        private void setupFrame() {
+            frame = new JFrame("GDP Data");
+            frame.setSize(500,500);
+            frame.setLocationRelativeTo(null);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+
+        private void buildPanels() {
+            mainPanel = new JPanel();
+
+            westPanel = new JPanel();
+            JPanel westTopPanel = new JPanel(new BorderLayout());
+            JPanel westBottomPanel = new JPanel();
+
+            westTopPanel.setPreferredSize(new Dimension(850, 400));
+            westTopPanel.add(tablePanel.getScrollPane(), BorderLayout.CENTER);
+            westTopPanel.add(part2.getFilters(), BorderLayout.NORTH);
+
+            westBottomPanel.setPreferredSize(new Dimension(850, 400));
+            westBottomPanel.setBackground(Color.WHITE);
+
+            westPanel.setPreferredSize(new Dimension(850, 1000));
+            westPanel.add(westTopPanel);
+            westPanel.add(westBottomPanel);
+
+            eastPanel = new JPanel();
+            JPanel eastTopPanel = new JPanel(new BorderLayout());
+            JPanel eastBottomPanel = new JPanel();
+
+            eastTopPanel.setPreferredSize(new Dimension(850, 400));
+            eastTopPanel.setBackground(Color.WHITE);
+            eastTopPanel.add(tablePanel.getAverageGDPLabel(), BorderLayout.SOUTH);
+
+            eastBottomPanel.setPreferredSize(new Dimension(850, 400));
+            eastBottomPanel.setBackground(Color.WHITE);
+
+            eastPanel.setPreferredSize(new Dimension(850, 1000));
+            eastPanel.add(eastTopPanel);
+            eastPanel.add(eastBottomPanel);
+        }
+
+        private void setPanels() {
+            mainPanel.add(westPanel);
+            mainPanel.add(eastPanel);
+        }
+
+        private void finalizegui() {
+            frame.add(mainPanel);
+            frame.setVisible(true);
         }
 }
