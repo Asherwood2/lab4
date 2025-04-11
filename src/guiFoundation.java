481cd05e7f934b2ff9ad874147a4d2f8577e0684
Austin Sherwood <asherwood349@cub.uca.edu>
2025-04-11 14:30:33 -0500

    Created class that will house the GUI

--- src/guiFoundation.java
+++ src/guiFoundation.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class guiFoundation {
+        private JFrame frame;
+        private JTableCalculation calculation;
+        private TablePanel tablePanel;
+        private Part2Panel part2;
+        private JPanel mainPanel, westPanel, eastPanel;
+
+        public guiFoundation() {
+            calculation = new JTableCalculation();
+            calculation.calculation();
+
+            tablePanel = new TablePanel(calculation.getColumns(), calculation.getData());
+            part2 = new Part2Panel(calculation.getListOfObjects());
+        }
 }
